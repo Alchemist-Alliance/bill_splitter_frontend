d94b259669d291b23dd70547069200cfed33ab3c
K0DEL <miketango2907@gmail.com>
2023-06-17 15:10:27 +0530

    created basic dashboard div layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,26 @@ import HeaderBar from './HeaderBar';
 function App() {
 
   return (
-    <HeaderBar />
+    <>
+      <HeaderBar />
+      <div className="wrapper">
+        <div>
+          <div className="current_trip">
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+          <div className="recent_expense">
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+    </>
+    
   )
 }
 
